96ec900062469f3698db29b7798a24893e43d576
Mahdi Yousefi <92528451+MMahdiYousefi@users.noreply.github.com>
2021-10-23 00:50:49 +0330

    Update MountainCar-DQN-Test.py

--- MC-DQN/MountainCar-DQN-Test.py
+++ MC-DQN/MountainCar-DQN-Test.py
@@ -1,1 +1,24 @@
+import time
+import gym
+import numpy as np
+from tensorflow.keras.models import load_model
 
+model = load_model('MountainCar-DQN_BestModel.h5')
+env = gym.make('MountainCar-v0').env
+
+EPISODES=5
+
+for e in range(EPISODES):
+    state = env.reset()
+    done = False
+    start = time.time()
+    while not done:
+        env.render()
+        state = np.reshape(state, (1,2))
+        action = model.predict(state)
+        action = np.argmax(action)
+        next_state, reward, done, info = env.step(action)
+        state = next_state
+        end = time.time()
+        ep_time = end - start
+    print(f'Episode time: {ep_time} seconds')
