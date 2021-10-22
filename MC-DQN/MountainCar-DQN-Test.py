f24b7574a244382e9b214a73953c09dfe6261809
Mahdi Yousefi <92528451+MMahdiYousefi@users.noreply.github.com>
2021-10-23 00:51:51 +0330

    Update MountainCar-DQN-Test.py

--- MC-DQN/MountainCar-DQN-Test.py
+++ MC-DQN/MountainCar-DQN-Test.py
@@ -3,11 +3,13 @@ import gym
 import numpy as np
 from tensorflow.keras.models import load_model
 
+# loading the saved model
 model = load_model('MountainCar-DQN_BestModel.h5')
 env = gym.make('MountainCar-v0').env
 
 EPISODES=5
 
+# create the loop that shows the result
 for e in range(EPISODES):
     state = env.reset()
     done = False
